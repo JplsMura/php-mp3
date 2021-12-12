48d1090306b7f5b0048a728d19339ab971de463e
João Pedro <joaopedrolima.s@hotmail.com>
2021-12-12 16:35:53 -0300

    Criando a listagem de musicas com base no album e reproduçao com a tag audio

--- public/pages/musics.php
+++ public/pages/musics.php
@@ -1,1 +1,17 @@
-teste
+<h1 class="mt-5">
+    Músicas do Álbum <?=$_GET['album']?>
+</h1>
+<hr>
+
+<?php
+    $album = $_GET['album'];
+    $musics = getMusics($album);
+
+    foreach($musics as $music):
+?>
+
+    <div class="col-12">
+        <audio src="<?=$music?>" controls></audio>
+    </div>
+
+<?php endforeach; ?>
