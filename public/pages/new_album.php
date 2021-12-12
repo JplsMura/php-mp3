9ce88c02397ad0dcfd69a780c5f1b4ec49c10322
João Pedro <joaopedrolima.s@hotmail.com>
2021-12-12 17:07:38 -0300

    Adicionando novo album

--- public/pages/new_album.php
+++ public/pages/new_album.php
@@ -1,6 +1,6 @@
 <h1 class="mt-5">Cadastro de Novo Álbum</h1>
 
-<form action="#" method="post">
+<form action="#" method="post" enctype="multipart/form-data">
 
     <div class="mb-3">
         <input type="text" name="name" id="name" placeholder="Name: " class="form-control">
@@ -13,3 +13,28 @@
     <button type="submit" class="btn btn-success">Cadastrar</button>
 
 </form>
+
+<?php
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+        $album = $_POST['name'];
+        $path = "albuns/{$album}";
+
+        if(!is_dir($path)){
+            mkdir($path);
+        }
+        
+        $file = $_FILES['imagem'];
+        $fileInfo = explode('.', $file['name']);
+
+        $extesion = $fileInfo[1];
+        $nameImage = $album . '.' . $extesion;
+
+        if(move_uploaded_file($file['tmp_name'], $path .'/'. $nameImage)){
+            header('Location: ?page=albuns');
+        }else
+            echo 'Falha no upload...';
+    }   
+
+?>
