48652823ebc847f683dfe7d80e6070eb71d4829a
João Pedro <joaopedrolima.s@hotmail.com>
2021-12-12 15:13:14 -0300

    Adicionando logica de listagem de albuns dinamico, com fotos e nome dos albuns

--- public/pages/albuns.php
+++ public/pages/albuns.php
@@ -3,15 +3,22 @@
 <div class="row">
 
     <?php
-        for($i = 1; $i <= 10; $i++):
+
+        $albuns = getAlbuns();
+
+        foreach ($albuns as $album):
+            $infoAlbum = explode('/', $album);
+            $nameAlbum = $infoAlbum[1];
+            // $imgAlbum = $album . '/' . $nameAlbum . '.jpg';
+            $imgAlbum = "{$album}/{$nameAlbum}.jpg";
     ?>
         <div class="col-3 album">
             <a href="">
-                <img class="img-album" src="albuns/sabotagem.jpg" alt="<?=$i?>">
-                <h6>Álbum <?=$i?></h6>
+                <img class="img-album" src="<?=$imgAlbum?>" alt="<?=$nameAlbum?>">
+                <h6><?=$nameAlbum?></h6>
             </a>
         </div>
     <?php
-        endfor;
+        endforeach;
     ?>
 </div>
